248a0025df7d900187c2ee9cf8cbda8461a42c00
drmack112 <69864665+drmack112@users.noreply.github.com>
2020-08-18 22:46:51 -0400

    Finish Nav
    Except for the color

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,9 +1,15 @@
-/* Set the width of the sidebar to 250px (show it) */
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementsByClassName("openbtn")[0].style.visibility = 'hidden';
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
-/* Set the width of the sidebar to 0 (hide it) */
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidepanel").style.width = "0";
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementsByClassName("openbtn")[0].style.visibility = 'visible';
+  document.body.style.backgroundColor = "white";
 }
